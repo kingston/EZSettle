795bbb902b3b6facb7e936b1e8cf7656ca390e8c
dtran320 <dtran320@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-05-11 20:08:15 +0000

    process pages

---
+++ ezsettle/process.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL); // show EVERYTHING
+require('init_smarty.php');
+
+$smarty->display('process.tpl');
+
+?>
+
---
+++ ezsettle/smarty/templates/process.tpl
@@ -0,0 +1,7 @@
+{include file="header.tpl"}
+
+<div class="main_body">
+
+</div><!-- end main body -->
+
+{include file="footer.tpl"}
