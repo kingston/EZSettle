77018a6616754023e454d048254e97fd7a749b2f
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-02 21:13:58 +0000

    update factcard

--- study/factcard.php
+++ study/factcard.php
@@ -3,33 +3,38 @@
 <HTML>
 
 <head>
-TATE
 
     <LINK href="style.css" rel="stylesheet" type="text/css">
 
-	<h1> Fact Card - Keep open for reference </h1>
+	<h1> Buyer's Facts for Mediation-Arbitration  - Keep open for reference </h1>
 
-	<h2> Buyer's Facts for Mediation-Arbitration </h2>
+	<h2>What Happened? </h2>
 
 </head>
 
-<body>  Your old laptop broke down the week before exams. A friend told you about the almost brand new laptop he bought for a great price from a seller who posted a sales listing on PC4U.com (an “e-bay” for PCs). It sounded like a great idea, and after browsing the PC4U.com website, you found a sales listing for a “like new” laptop that met your needs, for the price of $1000. Pleased, you purchased the laptop from the seller, using your credit card. Since you needed the laptop immediately you paid additional $20 for one day shipment. The package arrived the next day. When you took the laptop out of the box, you noticed that the screen was cracked and a few keyboard keys were loose. You contacted PC4U.com to complain about the condition of the laptop, and PC4U.com invited you to use their standard dispute resolution process, to which the seller also consented.  When you contacted PC4U.com, you did not mention two facts:
-<ul>
-	<li> You saw the delivery man carry the box to your house, and he seemed somewhat rough with it. You are not sure whether this could have caused any damage to the screen or keyboard, but it seems quite unlikely.  </li>
+<body><p>Your old laptop broke down the week before exams. A friend told you about the almost brand new laptop he bought for a great price from a seller who posted a sales listing on PC4U.com (an “e-bay” for PCs). It sounded like a great idea, and after browsing the PC4U.com website, you found a sales listing for a “like new” laptop that met your needs, for the price of $1000. Pleased, you purchased the laptop from the seller, using your credit card. Since you needed the laptop immediately you paid additional $20 for one day shipment. 
+</p>
+<p>The package arrived the next day. When you took the laptop out of the box, you noticed that the screen was cracked and a few keyboard keys were loose. You contacted PC4U.com to complain about the condition of the laptop, and PC4U.com invited you to use their standard dispute resolution process, to which the seller also consented. 
+</p>
 
-	<li> Although you are not sure ,you think you might be able to sell the laptop to your friend Gabe for about $300 (Gabe builds things out of computer parts, so he should not care that much about the cracked screen)  </li>
-</ul>At your discretion, you may or may not decide to share this additional information with the mediator and/or the other party.
+When you contacted PC4U.com, you did not mention two facts:
+<ul>
+	<li> You saw the delivery man carry the box to your house and he seemed somewhat rough with it. You are not sure whether this could have damaged the screen or keyboard.   </li>
+
+	<li> You might be able to sell the laptop to your friend Gabe for about $300 (Gabe needs computer parts for a project, and would not care that much about the cracked screen)   </li>
+</ul>At your discretion, you <b>may or may not</b> decide to share this additional information with the mediator and/or the other party (seller).
 
 <h2>What do you want to get out of the dispute resolution process?</h2>
 
-<ul>	<li>It seems unfair that you’ll be stuck with a computer you can’t really use, and you are annoyed you wasted your money on expedited shipping. You want to return the laptop to the seller and be refunded the full cost of the laptop ($1000) and shipping ($20). </li>	<li>Your uncle, who is a lawyer, said you are entitled for compensation for the emotional and financial damage you suffered due to not having a working laptop for exams.</li>	<ul>
-		<li>o	You had to rent a laptop for exams, costing $150, which was stressful and annoying. You feel it is only fair that the seller will bear this cost.</li>		<li>o	Your uncle says that you are entitled for compensation for the emotional distress you suffered. He estimates the standard compensation in such circumstances at $150.  You are not sure how it is calculated, but you do feel the seller should pay something for making you go through this nightmare during exam period. </li>
+<ul>	<li>You feel it is unfair that you are stuck with a computer you can’t really use, and you are annoyed that you wasted your money on expedited shipping. You want to return the laptop to the seller and receive a refund for the full cost of the laptop ($1000) and shipping ($20). </li>	<ul>
+		<li>Because you could not use the laptop you bought, you had to rent a laptop for the exam period for $150, it is only fair that the seller will bear this cost. </li>		<li>Your uncle, who is a lawyer, told you that you are entitled for compensation for the emotional distress you suffered. He estimates the standard compensation in such circumstances at $150. While you don’t know how he came up with this figure, you do feel the seller should pay something for making you go through this nightmare during exam period. </li>
 	</ul>
 </ul> 
 
 <h2>Here's a summary of the topics you need to address and your expenses:</h2>
 
-Laptop - $1000 <br>Expedited Shipping - $20 <br>Rental of replacement laptop - $150 <br>Emotional distress (your uncle's estimate) - $150 <br>Total - $1320
+<table><tr><td>Laptop</td><td>$1000</td></tr><tr><td>Expedited Shipping</td><td>$20</td></tr> <tr><td>Rental of replacement laptop</td><td>$150</td></tr><tr><td>Emotional distress (your uncle's estimate)</td><td>$150</td></tr><tr><td>Total</td><td>$1320</td></tr>
+</table>
 
 <br>
 <a href="http://ezsettle.dnsalias.com" target=blank>EZSettle</a><br>
