b88a36b952c1032f91f207f8e98112da9c813c3b
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-04 16:37:45 +0000

    add image

--- ezsettle/issues.php
+++ ezsettle/issues.php
@@ -5,8 +5,11 @@ require_once("include.php");
 $_SESSION['issuesArr']= array('Return laptop', 'Pay back cost of computer', 'Pay back original shipment', 'Pay cost of rental computer', 'Pay emotional distress damages', 'If relevant, who pays the cost of return shipment?');
 
 $_SESSION['chatNew'] = array(
-	array('speaker'=>'Mediator', 'message'=>'Hello!'), 
-	array('speaker' => 'Mediator', 'message' => 'In this process, we would like to address the concerns that both you and Casey345 raised. We combined the issues that the two of you included in your correspondence with PC4U.com into a single list.'), 
+	array('speaker' => 'EZSettle Mediator says', 'message' => 'In this process, we would like to address the concerns that you and
+Casey345 raised. We combined the issues that the two of you included in your correspondence
+with PC4U.com into a single list. You may not recognize all the issues in the list, if the other
+party raised an issue that you did not address. After reviewing the list, please click “yes” to
+confirm that these are the issues or “no” to add another issue.'), 
 	
 	array('speaker' => 'Mediator', 'message' => 'You may not recognize all the issues in the list, if the other party has raised an issue that you did not include in your list. After reviewing the list, please click “yes” to confirm that these are the issues or “no” to add another issue.'),
 	);
