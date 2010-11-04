acb2f5f939dbc5e0a779e184422a5ed6d3c0eda3
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-04 16:46:42 +0000

    add image

--- ezsettle/issues2.php
+++ ezsettle/issues2.php
@@ -5,7 +5,7 @@ require_once("include.php");
 $newOld = array_merge($_SESSION['chatOld'], $_SESSION['chatNew']);
 $_SESSION['chatOld'] = $newOld;
 $med_message = 'pull the marker left or right to indicate how important each issue is
-to you, on a scale from 1 (not important) to 100 (very important). Your ranking will help you think about your priorities. In case you and Casey345 do not reach agreement in mediation, the EZSettle ";
+to you, on a scale from 1 (not important) to 100 (very important). Your ranking will help you think about your priorities. In case you and Casey345 do not reach agreement in mediation, the EZSettle ';
 if(($_SESSION['condition']==1) || ($_SESSION['condition']==3)){
 	$med_message=$med_message.'arbitration software ';
 }else if (($_SESSION['condition']==2) || ($_SESSION['condition']==4)){
