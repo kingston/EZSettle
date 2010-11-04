250d93ad51ba243238d5db7d17e6fd1c85db7873
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-04 17:25:00 +0000

    add image

--- ezsettle/offer.php
+++ ezsettle/offer.php
@@ -30,6 +30,7 @@ switch($_SESSION['condition']) {
 	case 2:
 	case 5:
 		$goods = "goods";
+		$past_v2be = "were";
 		$ezsettle = "The system";
 		$mediator = "the system";
 		$ezsettle_me = "EZSettle";
@@ -38,6 +39,7 @@ switch($_SESSION['condition']) {
 	case 4:
 	case 5:
 		$goods = "laptop";
+		$past_v2be = "was";
 		$ezsettle = "I";
 		$mediator = "the mediator";
 		$ezssetle_me = "me";
@@ -84,7 +86,7 @@ switch($_SESSION['offer_num']) {
 	case 3:
 		$_SESSION['chatNew'] = array(
 									array( 'speaker' => 'Mediator',
-										'message' =>"You reported receiving the {$goods} you bought from Casey345 in an unacceptable condition. In e-commerce deals, it is sometimes the case that someone other than the buyer or the seller could have damaged the goods. Try recalling whether the package was open or otherwise damaged when you received it or if someone else received the package on your behalf. Consider whether something might have happened to the package between the time you received it and opened it or before or after you began using it. Use the scale below to mark how likely it is that the {$goods} vtobe damaged AFTER the seller has shipped it. {$ezsettle} will NOT share this information with Casey345"
+										'message' =>"You reported receiving the {$goods} you bought from Casey345 in an unacceptable condition. In e-commerce deals, it is sometimes the case that someone other than the buyer or the seller could have damaged the goods. Try recalling whether the package was open or otherwise damaged when you received it or if someone else received the package on your behalf. Consider whether something might have happened to the package between the time you received it and opened it or before or after you began using it. Use the scale below to mark how likely it is that the {$goods} {$past_v2be} damaged AFTER the seller has shipped it. {$ezsettle} will NOT share this information with Casey345"
 									)
 								);
 		break;
@@ -183,6 +185,7 @@ $smarty->assign('condition', $_SESSION['condition']);
 $smarty->assign('avatar', $avatar);
 $smarty->assign('mediator', $mediator);
 $smarty->assign('mediator_caps', $mediator_caps);
+$smarty->assign('past_v2be',$past_v2be);
 $smarty->assign('step', $step);
 //var_dump($_SESSION['counteroffers'][$step]);
 //Your offer
