90230894caacd1973b453905a371ca97498a6f8a
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-04 16:48:24 +0000

    add image

--- ezsettle/issues2.php
+++ ezsettle/issues2.php
@@ -4,8 +4,7 @@ require_once("include.php");
 
 $newOld = array_merge($_SESSION['chatOld'], $_SESSION['chatNew']);
 $_SESSION['chatOld'] = $newOld;
-$med_message = 'pull the marker left or right to indicate how important each issue is
-to you, on a scale from 1 (not important) to 100 (very important). Your ranking will help you think about your priorities. In case you and Casey345 do not reach agreement in mediation, the EZSettle ';
+$med_message = 'pull the marker left or right to indicate how important each issue is to you, on a scale from 1 (not important) to 100 (very important). Your ranking will help you think about your priorities. In case you and Casey345 do not reach agreement in mediation, the EZSettle ';
 if(($_SESSION['condition']==1) || ($_SESSION['condition']==3)){
 	$med_message=$med_message.'arbitration software ';
 }else if (($_SESSION['condition']==2) || ($_SESSION['condition']==4)){
@@ -13,8 +12,7 @@ if(($_SESSION['condition']==1) || ($_SESSION['condition']==3)){
 }else{
 	$med_message=$med_message.'arbitrator';
 }
-$med_message=$med_message.'will take into account
-which issues are more important to you when determining the final settlement.'
+$med_message=$med_message.'will take into account which issues are more important to you when determining the final settlement.';
 $_SESSION['chatNew'] = array(
 							array('speaker' => 'EZSettle Mediator says', 
 								'message' => $med_message)
