253d32f5f8234e8845d0b334a5ac61d370372173
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-09 22:49:40 +0000

    Change the study website

--- study/index.php
+++ study/index.php
@@ -8,17 +8,59 @@
 	
     <LINK href="style.css" rel="stylesheet" type="text/css">
 
-	<h1> Welcome! </h1>
+	<h1>Welcome to the Online Dispute Resolution Study!</h1>
 
 </head>
 
 <body> 
-	<p>In this study, you and another participant will each be asked to play the role of a person who has been involved in a transaction online in which something has gone wrong. Each one of you has been randomly assigned to the role of either the buyer or the seller in the transaction, and you will receive a fact sheet describing your respective stories. The fact sheet will describe what has happened and will provide you with the necessary information for the resolution of the dispute. After you have read your role, we will ask you to attempt to resolve the case using a new system of online dispute resolution that we are testing: EZSettle.com.  On the EZSettle website you will find explanations about how the system works. In order to simulate a real case, we provide you with some general information about your side of the story. We ask that you will use this information in the same way you would have used it had you found yourself in such a situation in real life. After you complete the process using EZSettle.com, we will ask you to give us feedback on the system and will ask you some questions about your experience. In total, the study has 3 parts: fill personal data, go to website of system, and fill feedback.
+
+	<p>
+	This study is intended to test a new service called <i>EZSettle</i>.
 	</p>
+	<p>
+	You and another participant will each be playing the role of a person who has been involved in
+	an e-commerce transaction in which something has gone wrong. You were randomly assigned
+	to the role of either the buyer or the seller in the transaction. You will receive a fact sheet that
+	describes your side of the story to provide you with information for attempting to resolve the
+	dispute that emerged.
+	</p>
+	<p>
+	After you have each read your role, you will be asked to enter the EZSettle website. EZSettle is
+	a new service for resolving disputes online. The instructions for the EZSettle process are detailed
+	on the EZSettle website. It is essential that you read the instructions thoroughly.
+	</p>
+	<p>
+	After resolving the dispute using EZSettle, we will ask you to give us feedback on the process by
+	asking you some questions about your experience.
+	</p>
+	
+	<h3>The study has 3 parts</h3>
+	<ol>
+		<li>Read the fact sheet</li>
+		<li>Use EZSettle</li>
+		<li>Fill out the feedback questionnaire</li>
+	<ol>	
+	<p>
+	The study is expected to take a total of 90 minutes, and you will receive 2 hours of credit for it.
+	</p>
+	<br/>
+	<div style="border:1px solid black;">
+	<h3>IMPORTANT</h3>
+		<p>
+			To simulate a real case, we ask that you carefully read the fact sheet. You should use EZSettle in
+			the same way you would have used it had you found yourself in the described situation in real
+			life.
+		</p>
+		<p>
+			As a token of our appreciation, we will reward your diligent and attentive participation by
+			entering you into a drawing for ten $25 Amazon gift cards.
+		</p>
+	</div>
+	
 </body>
 
 <p>
-	<a href="info.php"> Continue </a>
+	<a href="instructions.php"> Continue </a>
 </p>
 
 <HTML>
