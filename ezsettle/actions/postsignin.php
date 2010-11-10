22ccffd8b976f49c2ba98ec05de2b578de23d6d8
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-10 16:53:59 +0000

    arbitration welcome page

--- ezsettle/actions/postsignin.php
+++ ezsettle/actions/postsignin.php
@@ -16,7 +16,7 @@ else {
 $result = array('success' => true);
 //connect to db
 try {
-    	$db = new PDO("sqlite:../db/test.db" );
+    	$db = new PDO("sqlite:../db/ezsettle.db" );
     	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     	//echo "PDO connection object created";
     } catch(PDOException $e){
