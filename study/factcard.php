0f5156edfb2db72d0036ddf4c524c787908feb40
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-10 02:35:27 +0000

    arbitration welcome page

--- study/factcard.php
+++ study/factcard.php
@@ -23,29 +23,33 @@
 </head>
 
 <body>
-	<h2>What Happened? </h2><p>Your old laptop broke down the week before exams. A friend told you about the almost brand new laptop he bought for a great price from a seller who posted a sales listing on PC4U.com (an &quot;e-bay&quot; for PCs). It sounded like a great idea, and after browsing the PC4U.com website, you found a sales listing for a &quot;like new&quot; laptop that met your needs, for the price of $1000. Pleased, you purchased the laptop from the seller, using your credit card. Since you needed the laptop immediately you paid additional $20 for one day shipment. 
+	<h2>What Happened? </h2><p>
+Your old laptop broke down the week before exams. A friend told you about the almost brand new laptop he bought for a great price on PC4U.com. He explained that PC4U.com is a website that helps persons interested in selling or buying used computers to find each other and make deals. It sounded like a great idea, and after browsing the PC4U.com website, you found a sales listing for a &quot;like new&quot; laptop that met your needs. Pleased, you purchased the laptop from the seller for $1000, using your credit card. Since you needed the laptop immediately you paid additional $25 to have the laptop delivered to you within 24 hours. 
 </p>
-<p>The package arrived the next day. When you took the laptop out of the box, you noticed that the screen was cracked and a few keyboard keys were loose. You contacted PC4U.com to complain about the condition of the laptop, and PC4U.com invited you to use their standard dispute resolution process, to which the seller also consented. 
+<p>The package arrived the next day. When you took the laptop out of the box, you noticed that the screen was cracked and a few keyboard keys were loose. You contacted PC4U.com to complain about the condition of the laptop, and PC4U.com invited you to use their standard dispute resolution process. You and the seller both agreed to use this standard dispute resolution process run by a company called EZSettle 
 </p>
 
 When you contacted PC4U.com, you did not mention two facts:
 <ul>
-	<li> You saw the delivery man carry the box to your house and he seemed somewhat rough with it. You are not sure whether this could have damaged the screen or keyboard.   </li>
+	<li>You happened to see the delivery man carry the box to your house. He seemed somewhat rough with it. You are not sure whether this could have damaged the screen or keyboard.    </li>
 
-	<li> You might be able to sell the laptop to your friend Gabe for about $300 (Gabe needs computer parts for a project, and would not care that much about the cracked screen)   </li>
-</ul>At your discretion, you <b>may or may not</b> decide to share this additional information with the mediator and/or the other party (seller).
+	<li> Your friend Rory needs computer parts for a project, and is willing to pay you $300 for the computer. Rory knows that the screen is cracked and that a few keys are loose, and doesn’t care.   </li>
+</ul>At your discretion, you may decide whether or not to share these two facts in the dispute resolution process.
 
 <h2>What do you want to get out of the dispute resolution process?</h2>
-	<p>You feel it is unfair that you are stuck with a computer you cannot really use, and you are annoyed that you wasted your money on expedited shipping. You want to return the laptop to the seller and receive a refund for the full cost of the laptop ($1000) and shipping ($20). </p>	<ul>
-		<li>Because you could not use the laptop you bought, you had to rent a laptop for the exam period for $150, it is only fair that the seller will bear this cost. </li>		<li>Your uncle, who is a lawyer, told you that you are entitled for compensation for the emotional distress you suffered. He estimates the standard compensation in such circumstances at $150. While you do not know how he came up with this figure, you do feel the seller should pay something for making you go through this nightmare during exam period. </li>
+	<p>You feel it is unfair that you are stuck with a computer you can’t really use, and you are annoyed that you wasted your money on expedited shipping. You want to return the laptop to the seller and receive a refund for the full cost of the laptop ($1000) and shipping ($25).  </p>
+	<p>In addition, there are a few other costs that you would like to address: </p>	<ul>
+		<li>Because you could not use the laptop you bought, you had to pay $150 to rent a computer for the exam period. It is only fair that the seller will be responsible for this cost.  </li>		<li>Your uncle, who is a lawyer, told you that you are entitled for compensation for the emotional distress you suffered due to the situation. He estimated the standard compensation in such circumstances was $150. You don’t know how he came up with this figure, but you trust him and you feel the seller should pay something for making you go through this nightmare during exam period.  </li>
 	</ul>
 
-<h2>Here's a summary of the topics you need to address and your expenses:</h2>
+<h2>This is a summary of the costs you want to recover in the negotiation:</h2>
 
-<table><tr><td>Laptop</td><td>$1000</td></tr><tr><td>Expedited Shipping</td><td>$20</td></tr> <tr><td>Rental of replacement laptop</td><td>$150</td></tr><tr><td>Emotional distress (your uncle's estimate)</td><td>$150</td></tr><tr><td>Total</td><td>$1320</td></tr>
+<table><tr><td>Laptop</td><td>$1000</td></tr><tr><td>Expedited Shipping</td><td>$25</td></tr> <tr><td>Rental of replacement laptop</td><td>$150</td></tr><tr><td>Emotional distress (your uncle's estimate)</td><td>$150</td></tr><tr><td>Total</td><td>$1325</td></tr>
 </table>
-
-<br>
+<p>
+You will now have the opportunity to go through a mediation-arbitration process to discuss this situation.
+</p>
+<br/ >
 <a href="http://ezsettle.dnsalias.com" target=blank>EZSettle</a><br>
 We recommend opening EZSettle in a separate window instead of tab.
 
