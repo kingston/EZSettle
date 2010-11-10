4377fe1e7d72dac33f9f7817f8f948c391b64cf3
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-10 17:03:08 +0000

    arbitration welcome page

--- ezsettle/actions/postsignin.php
+++ ezsettle/actions/postsignin.php
@@ -25,7 +25,7 @@ try {
 
 
 $now = date('y-m-d H:i:s',time());
-	$sql = "INSERT INTO users (user_id, condition, login_time,)".
+	$sql = "INSERT INTO users (user_id, condition, login_time)".
 		" VALUES (".$_SESSION['experimental_id'].", ".$_SESSION['condition'].",".$now.")";
 	try {
     	$count = $db->exec($sql);
