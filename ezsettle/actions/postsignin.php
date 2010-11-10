08039d02e41cac95af6190179d9377438f9154a9
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-10 16:30:41 +0000

    arbitration welcome page

--- ezsettle/actions/postsignin.php
+++ ezsettle/actions/postsignin.php
@@ -16,7 +16,7 @@ else {
 $result = array('success' => true);
 
 //connect to db
-/*try {
+try {
     	$db = new PDO("sqlite:db/ezsettle.db" );
     	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     	//echo "PDO connection object created";
@@ -37,24 +37,3 @@ $result = array('success' => true);
 echo json_encode($result);
 
 ?>
-
-------------
- //ini_set('display_errors', 1);                                                              
- //error_reporting(E_ALL);
-	try {
-    	$db = new PDO("sqlite:../db/test.db" );
-    	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	//echo "PDO connection object created";
-    } catch(PDOException $e){
-    	echo $e->getMessage();
-    }
-
-	$sql = "INSERT INTO faqs (user_fk, time_spent) VALUES (".$_POST['user'].", ".$_POST['time_spent'].")";
-
-	try {
-    	$count = $db->exec($sql);
-    } catch(PDOException $e){
-    	echo $e->getMessage();
-    }
-
-	//echo $count;
