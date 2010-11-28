7f3195cbe023c40f1464d6bd21d049c2c2617397
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-28 16:49:51 +0000

    study web

--- ezsettle/issues.php
+++ ezsettle/issues.php
@@ -5,7 +5,7 @@ require_once("include.php");
 $_SESSION['issuesArr']= array('Return laptop', 'Pay back cost of computer', 'Pay back original shipment', 'Pay cost of rental computer', 'Pay emotional distress damages', 'If relevant, who pays the cost of return shipment?');
 
 $_SESSION['chatNew'] = array(
-	array('speaker' => 'Mediator', 'message' => 'In this process, {if $condition == 1 || $condition == 2 || $condition == 5}the system{else}I{/if} would like to address the concerns that you and
+	array('speaker' => 'Mediator', 'message' => 'In this process, {$I} would like to address the concerns that you and
 Casey345 raised. EZSettle combined the issues that the two of you
 included in your correspondence with PC4U.com into a single list. You may not recognize all
 the issues in the list: the other party may have raised an issue that you did not address. After
@@ -18,11 +18,13 @@ switch($_SESSION['condition']) {
 	case 2:
 	case 5:
 		$typing = "The system is generating text ...";
+		$I = "the system";
 		break;
 	case 3:
 	case 4:
 	case 6:
 		$typing = "The mediator is typing ... ";
+		$I = "I";
 		break;
 }
 
@@ -34,6 +36,7 @@ $smarty->assign('chatOld', $_SESSION['chatOld']);
 $smarty->assign('condition', $_SESSION['condition']);
 $smarty->assign('avatar', $avatar);
 $smarty->assign('typing',$typing);
+$smarty->assign('I', $I);
 $smarty->display('issues.tpl');
 
 
