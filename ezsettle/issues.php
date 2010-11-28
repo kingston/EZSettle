3c27c196b322e21d2b657dffc5e1cb4802b55ac3
ksiangliulue <ksiangliulue@295da97a-6570-4c7c-1996-da3e9cecc61d>
2010-11-28 16:51:03 +0000

    study web

--- ezsettle/issues.php
+++ ezsettle/issues.php
@@ -2,17 +2,6 @@
 
 require_once("include.php");
 
-$_SESSION['issuesArr']= array('Return laptop', 'Pay back cost of computer', 'Pay back original shipment', 'Pay cost of rental computer', 'Pay emotional distress damages', 'If relevant, who pays the cost of return shipment?');
-
-$_SESSION['chatNew'] = array(
-	array('speaker' => 'Mediator', 'message' => 'In this process, {$I} would like to address the concerns that you and
-Casey345 raised. EZSettle combined the issues that the two of you
-included in your correspondence with PC4U.com into a single list. You may not recognize all
-the issues in the list: the other party may have raised an issue that you did not address. After
-reviewing the list, please click “yes” to confirm that these are the issues or “no” to add another
-issue.')
-	);
-	
 switch($_SESSION['condition']) {
 	case 1:
 	case 2:
@@ -28,6 +17,19 @@ switch($_SESSION['condition']) {
 		break;
 }
 
+$_SESSION['issuesArr']= array('Return laptop', 'Pay back cost of computer', 'Pay back original shipment', 'Pay cost of rental computer', 'Pay emotional distress damages', 'If relevant, who pays the cost of return shipment?');
+
+$_SESSION['chatNew'] = array(
+	array('speaker' => 'Mediator', 'message' => 'In this process,".$I."would like to address the concerns that you and
+Casey345 raised. EZSettle combined the issues that the two of you
+included in your correspondence with PC4U.com into a single list. You may not recognize all
+the issues in the list: the other party may have raised an issue that you did not address. After
+reviewing the list, please click “yes” to confirm that these are the issues or “no” to add another
+issue.')
+	);
+	
+
+
 $_SESSION['chatOld'] = array();
 $smarty->assign('username', sessionVar('username'));
 $smarty->assign('issues', $_SESSION['issuesArr']);
@@ -36,7 +38,6 @@ $smarty->assign('chatOld', $_SESSION['chatOld']);
 $smarty->assign('condition', $_SESSION['condition']);
 $smarty->assign('avatar', $avatar);
 $smarty->assign('typing',$typing);
-$smarty->assign('I', $I);
 $smarty->display('issues.tpl');
 
 
